85bb35b4a78b11deb28a5da3662250b178dcbb11
Evgenia <eug.gross@yandex.ru>
2026-05-13 00:05:00 +0300

    rename GenreBaseSerializer to GenreResponse

--- src/schemas/genres.py
+++ src/schemas/genres.py
@@ -2,6 +2,6 @@ from uuid import UUID
 from pydantic import BaseModel, Field
 
 
-class GenreBaseSerializer(BaseModel):
+class GenreResponse(BaseModel):
     uuid: UUID = Field(validation_alias="id")
     name: str
