2c658db2a60c66db4007ba39a5870e5f9f2ff6af
EugeniaGross <63613806+EugeniaGross@users.noreply.github.com>
2026-05-13 12:59:33 +0300

    add Dockerfile to src and add docker-compose and add middleware (#2)
    * add Dockerfile to src and add docker-compose and add middleware
    
    * fixed infra
    
    * fixed src/Dockerfile
    
    Closes #2

--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@ from contextlib import asynccontextmanager
 
 from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi.responses import ORJSONResponse
 from redis.asyncio import Redis
+from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
 
 from api.v1 import films
 from core import config
@@ -28,5 +31,17 @@ app = FastAPI(
     default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
+
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=config.ALLOW_HOSTS)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=config.ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(ProxyHeadersMiddleware, trusted_hosts=config.ALLOW_HOSTS)
 
 app.include_router(films.router, prefix="/api/v1/films", tags=["films"])
